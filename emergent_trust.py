bb9782a3255ee37070ffa68ae07352947af16209
adelina_notebook <isaeva.adelya2016@ya.ru>
2023-08-01 02:33:59 +0300

    graph's edge and vert done, but weight...

--- emergent_trust.py
+++ emergent_trust.py
@@ -2,65 +2,98 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import random
 
-# Создаем граф с 10 узлами и соединяем каждый узел с двумя соседями
-G = nx.Graph()
-n_nodes = 10 # count of agents
-G.add_nodes_from(range(n_nodes))
-for i in range(n_nodes):
-    G.add_edge(i, (i + 1) % n_nodes)
-    G.add_edge(i, (i + 2) % n_nodes)
+from main import Agent, Trust
 
-# Инициализируем доверие случайными значениями
-for u, v in G.edges():
-    G[u][v]['trust'] = random.random() #TODO how this get
 
 # Функция для обновления доверия на основе взаимодействий агентов
-def update_trust(G, iterations=100):
-    for _ in range(iterations):
-        u, v = random.sample(G.edges(), 1)[0]
-        interaction_result = random.random() > 0.5
+def update_trust(G, iterations):
+    for interaction in iterations:
+        u = interaction.get_agent1()
+        v = interaction.get_agent2()
+        # my vision TODO
+        interaction_result = interaction.get_sentiment() * \
+            interaction.get_interaction_type() * u.reputation * v.reputation
         trust_change = 0.1 if interaction_result else -0.1
         G[u][v]['trust'] = min(max(G[u][v]['trust'] + trust_change, 0), 1)
 
-# Анализируем эмерджентное доверие в графе
-update_trust(G)
 
-# Визуализируем граф с помощью цвета и толщины ребер, основанных на доверии
-pos = nx.circular_layout(G)
-edge_colors = [G[u][v]['trust'] for u, v in G.edges()]
-edge_widths = [G[u][v]['trust'] * 3 for u, v in G.edges()]
-nx.draw(G, pos, node_size=500, node_color='blue', with_labels=True,
-        edge_color=edge_colors, edge_cmap=plt.cm.Reds, width=edge_widths)
-plt.show()
-def emergent_trust(G):
-    total_interaction_trust = 0
-    for u, v in G.edges():
-        total_interaction_trust += G[u][v]['interaction_count'] * G[u][v]['trust']
+def draw_graph(agents, trusts, interactions=100):
+    # Создаем граф с 10 узлами и соединяем каждый узел с двумя соседями
+    G = nx.Graph()
+    G.add_nodes_from(agents)
+    for trust in trusts:
+        G.add_edge(trust.get_agent1(),
+                   trust.get_agent2())
+    print(G)
+    # n_nodes = agents.count()
+    # G.add_nodes_from(range(n_nodes))
+    # for i in range(n_nodes):
+    #     G.add_edge(i, (i + 1) % n_nodes)
+    #     G.add_edge(i, (i + 2) % n_nodes)
 
-    emergent_trust = dict()
-    for u, v in G.edges():
-        emergent_trust[(u, v)] = (G[u][v]['interaction_count'] * G[u][v]['trust']) / total_interaction_trust
+    # Инициализируем доверие случайными значениями
+    for u in G.nodes():
+        for v in G.nodes():
+            if not v == u:
+                print(u.get_trust_score_by_id(v.ID), u, v )
+        # G[u][v]['trust'] = float(u.get_trust_score_by_id(v.ID))
+    # Анализируем эмерджентное доверие в графе
+    # update_trust(G, interactions)
+    # Визуализируем граф с помощью цвета и толщины ребер, основанных на доверии
+    pos = nx.circular_layout(G)
+    # edge_colors = [G[u][v]['trust'] for u, v in G.edges()]
+    # edge_widths = [G[u][v]['trust'] * 3 for u, v in G.edges()]
+    nx.draw(G, pos, )#node_size=500, node_color='blue', with_labels=True,
+           # edge_color=edge_colors, edge_cmap=plt.cm.Reds, width=edge_widths)
+    plt.show()
 
-    return emergent_trust
 
-# Создаем граф и инициализируем ребра с атрибутами 'interaction_count' и 'trust'
-# G = nx.Graph()
-# G.add_edges_from([(0, 1), (1, 2), (2, 3), (3, 4)])
-for u, v in G.edges():
-    G[u][v]['interaction_count'] = random.randint(1, 10)
-    # G[u][v]['trust'] = random.random()
+agents = [Agent(1),
+          Agent(2, "123", 0.6), Agent(3, "qwe", 0.1)]
+trusts = [Trust(agents[0], agents[1], 0.5),
+          Trust(agents[0], agents[2], 0.6),
+          Trust(agents[2], agents[1], 0.1)
+          ]
+print(agents[0].get_trust_scores(), trusts[2]
+      )
+draw_graph(agents, trusts)
 
-# Вычисляем эмерджентное доверие
-emergent_trust_values = emergent_trust(G)
-print("Вычисляем эмерджентное доверие:")
-print(emergent_trust_values)
-def average_emergent_trust(G):
-    emergent_trust_values = emergent_trust(G)
-    total_emergent_trust = sum(emergent_trust_values.values())
-    num_edges = len(G.edges())
+# interaptions =
+# agents[0].add_trust_score(Tru)
 
-    return total_emergent_trust / num_edges
 
-# Вычисляем среднее эмерджентное доверие
-average_ED = average_emergent_trust(G)
-print("Среднее эмерджентное доверие:", average_ED)
+
+
+
+# def emergent_trust(G):
+#     total_interaction_trust = 0
+#     for u, v in G.edges():
+#         total_interaction_trust += G[u][v]['interaction_count'] * G[u][v]['trust']
+#
+#     emergent_trust = dict()
+#     for u, v in G.edges():
+#         emergent_trust[(u, v)] = (G[u][v]['interaction_count'] * G[u][v]['trust']) / total_interaction_trust
+#
+#     return emergent_trust
+#
+# # Создаем граф и инициализируем ребра с атрибутами 'interaction_count' и 'trust'
+# # G = nx.Graph()
+# # G.add_edges_from([(0, 1), (1, 2), (2, 3), (3, 4)])
+# for u, v in G.edges():
+#     G[u][v]['interaction_count'] = random.randint(1, 10)
+#     # G[u][v]['trust'] = random.random()
+#
+# # Вычисляем эмерджентное доверие
+# emergent_trust_values = emergent_trust(G)
+# print("Вычисляем эмерджентное доверие:")
+# print(emergent_trust_values)
+# def average_emergent_trust(G):
+#     emergent_trust_values = emergent_trust(G)
+#     total_emergent_trust = sum(emergent_trust_values.values())
+#     num_edges = len(G.edges())
+#
+#     return total_emergent_trust / num_edges
+#
+# # Вычисляем среднее эмерджентное доверие
+# average_ED = average_emergent_trust(G)
+# print("Среднее эмерджентное доверие:", average_ED)
--- main.py
+++ main.py
@@ -6,12 +6,14 @@ class Agent:
     def __init__(self, agent_id, name="baseName", reputation=0.50):
         self.ID = agent_id
         self.name = name
-        self.reputation = reputation #interval
-        self._trustScores = []  # БЫЛО random.randint(0, 100)   TODO это массив по идее
+        self.reputation = reputation  # interval
+        self.trustScores = []  # БЫЛО random.randint(0, 100)   TODO это массив по идее
         # однако я думаю что это отдельная сущность характеризующ отношения между агентами
         # and what interval
-        self._interactions = []
+        self.interactions = []
 
+    def __str__(self):
+        return self.ID.__str__() + "." + self.name
     def get_ID(self):
         return self.ID
 
@@ -35,8 +37,8 @@ class Agent:
 
     def get_trust_score_by_id(self, agent_id):
         for item in self.trustScores:
-            if item.agent2 == agent_id:
-                return item.score
+            if item.get_agent2().ID == agent_id or item.get_agent1().ID == agent_id:
+                return item.get_score()
             else:
                 return None
 
@@ -49,7 +51,7 @@ class Agent:
     def get_interactions_by_id(self, agent_id):
         items = []
         for item in self.interactions:
-            if item.agent2 == agent_id or item.agent1 == agent_id:
+            if item.agent2.ID == agent_id or item.agent1.ID == agent_id:
                 items.append(item)
         return items
 
@@ -86,6 +88,14 @@ class Trust:
         self._agent1 = agent1
         self._agent2 = agent2
         self._score = score
+        agent1.add_trust_score(self)
+        agent2.add_trust_score(self)
+
+    def __str__(self):
+        return self._agent1.__str__() + "~" + self._agent2.__str__() + ":" \
+            + self._score.__str__()
+    # def __len__(self):
+    #     return int(self._score * 10)
 
     def get_agent1(self):
         return self._agent1
@@ -103,14 +113,14 @@ class Trust:
 class EmergentTrust:
     @staticmethod
     def calculate_trust(agent_i, agent_j, agents):
-        trust_score = agent_i.get_trust_score_by_id(agent_id=agent_j)
-        interaction_count = agent_i.get_interactions_by_id(agent_id=agent_j).count()
+        trust_score = agent_i.get_trust_score_by_id(agent_id=agent_j.ID)
+        interaction_count = agent_i.get_interactions_by_id(agent_id=agent_j.ID).count()
         sum = 0
         for x in agents:
             for y in agents:
-                if not x == y:
-                    sum = x.get_interactions_by_id(agent_id=y).count() * \
-                          x.get_trust_score_by_id(agent_id=y)
+                if not x.ID == y.ID:
+                    sum = x.get_interactions_by_id(agent_id=y.ID).count() * \
+                          x.get_trust_score_by_id(agent_id=y.ID)
         sum /= 2
         return trust_score * interaction_count / sum
         # (InteractionCount(i, j) * TrustScore(i, j)) / Sum(InteractionCount(x, y) * TrustScore(x, y))
@@ -118,76 +128,76 @@ class EmergentTrust:
         # (Implementation left to your specific requirements)
         pass
 
-
-from dostoevsky.tokenization import RegexTokenizer
-from dostoevsky.models import FastTextSocialNetworkModel
-
-tokenizer = RegexTokenizer()
-tokens = tokenizer.split('всё очень плохо')  # [('всё', None), ('очень', None), ('плохо', None)]
-
-model = FastTextSocialNetworkModel(tokenizer=tokenizer)
-
-
-messages = [
-    'Сегодня хорошая погода',
-    'Я счастлив проводить с тобою время',
-    'Мне нравится эта музыкальная композиция',
-    'В больнице была ужасная очередь',
-    'Сосед с верхнего этажа мешает спать',
-    'Маленькая девочка потерялась в торговом центре',
-]
-
-results = model.predict(messages, k=6)
-
-for message, sentiment in zip(messages, results):
-    # привет -> {'speech': 1.0000100135803223, 'skip': 0.0020607432816177607}
-    # люблю тебя!! -> {'positive': 0.9886782765388489, 'skip': 0.005394937004894018}
-    # малолетние дебилы -> {'negative': 0.9525841474533081, 'neutral': 0.13661839067935944}]
-    print(message, '->', sentiment)
-
-
-def generate_data_from_kt():
-    # generate agents (A...Z)
-    import csv
-    agents_data = [
-        {"ID": 1, "name": "A", "reputation": 0.50},
-        {"ID": 2, "name": "B", "reputation": 0.50}
-    ]
-    fieldnames = ["ID", "name", "reputation"]
-    with open('agents.csv', 'w', newline='') as csvfile:
-        csv_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        csv_writer.writeheader()
-        csv_writer.writerows(agents_data)
-
-
-
-
-def read_agents_from_csv(filename="agents.csv"):
-    agents = []
-    with open(filename, newline='') as csvfile:
-        csv_reader = csv.DictReader(csvfile)
-        for row in csv_reader:
-            ID = int(row['ID'])
-            name = row['name']
-            reputation = float(row['reputation'])
-            agent = Agent(agent_id=ID, name=name, reputation=reputation)
-            agents.append(agent)
-
-    for agent in agents:
-        print(f"ID: {agent.ID}, Name: {agent.name}, Reputation: {agent.reputation}")
-
-    return agents
-
-
-
-
-with open('kt.csv', newline='', encoding='utf8') as csvfile:
-
-    csv_reader = csv.reader(csvfile)
-
-    for row in csv_reader:
-        # 0 - id отзыва
-        # 1 - содержимое (rus)
-        # 3 - оценка (good, bad, neutral)
-        #print(row[3])
-        pass
+#
+# from dostoevsky.tokenization import RegexTokenizer
+# from dostoevsky.models import FastTextSocialNetworkModel
+#
+# tokenizer = RegexTokenizer()
+# tokens = tokenizer.split('всё очень плохо')  # [('всё', None), ('очень', None), ('плохо', None)]
+#
+# model = FastTextSocialNetworkModel(tokenizer=tokenizer)
+#
+#
+# messages = [
+#     'Сегодня хорошая погода',
+#     'Я счастлив проводить с тобою время',
+#     'Мне нравится эта музыкальная композиция',
+#     'В больнице была ужасная очередь',
+#     'Сосед с верхнего этажа мешает спать',
+#     'Маленькая девочка потерялась в торговом центре',
+# ]
+#
+# results = model.predict(messages, k=6)
+#
+# for message, sentiment in zip(messages, results):
+#     # привет -> {'speech': 1.0000100135803223, 'skip': 0.0020607432816177607}
+#     # люблю тебя!! -> {'positive': 0.9886782765388489, 'skip': 0.005394937004894018}
+#     # малолетние дебилы -> {'negative': 0.9525841474533081, 'neutral': 0.13661839067935944}]
+#     print(message, '->', sentiment)
+#
+#
+# def generate_data_from_kt():
+#     # generate agents (A...Z)
+#     import csv
+#     agents_data = [
+#         {"ID": 1, "name": "A", "reputation": 0.50},
+#         {"ID": 2, "name": "B", "reputation": 0.50}
+#     ]
+#     fieldnames = ["ID", "name", "reputation"]
+#     with open('agents.csv', 'w', newline='') as csvfile:
+#         csv_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+#         csv_writer.writeheader()
+#         csv_writer.writerows(agents_data)
+#
+#
+#
+#
+# def read_agents_from_csv(filename="agents.csv"):
+#     agents = []
+#     with open(filename, newline='') as csvfile:
+#         csv_reader = csv.DictReader(csvfile)
+#         for row in csv_reader:
+#             ID = int(row['ID'])
+#             name = row['name']
+#             reputation = float(row['reputation'])
+#             agent = Agent(agent_id=ID, name=name, reputation=reputation)
+#             agents.append(agent)
+#
+#     for agent in agents:
+#         print(f"ID: {agent.ID}, Name: {agent.name}, Reputation: {agent.reputation}")
+#
+#     return agents
+#
+#
+#
+#
+# with open('kt.csv', newline='', encoding='utf8') as csvfile:
+#
+#     csv_reader = csv.reader(csvfile)
+#
+#     for row in csv_reader:
+#         # 0 - id отзыва
+#         # 1 - содержимое (rus)
+#         # 3 - оценка (good, bad, neutral)
+#         #print(row[3])
+#         pass
